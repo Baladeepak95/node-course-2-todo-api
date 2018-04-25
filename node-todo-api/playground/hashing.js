9bca88d976366bcb5132f2777ded61a9df1df75a
unknown <bala@enunui.com>
2018-04-25 15:24:17 +0530

    Hash user password before saving

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
-var token= jwt.sign(data,'123abc');
-console.log(token);
+var password = '123abc';
 
-var Decoded = jwt.verify(token, '123abc');
-console.log('Decoded:',Decoded);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$I5Hu8SSlPpD.SePQVJ9cZeFcZ2/Avf3gt6Hn3107T1e3fsqy6R1/G';
+
+bcrypt.compare('123', hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+// var data = {
+//     id:10
+// };
+// var token= jwt.sign(data,'123abc');
+// console.log(token);
+
+// var Decoded = jwt.verify(token, '123abc');
+// console.log('Decoded:',Decoded);
 // var message = 'I am  0';
 // var hash = SHA256(message).toString();
 
