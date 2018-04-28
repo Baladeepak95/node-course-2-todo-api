d3b8f83443d3ee7762a5f598d81fbf922b6b1282
unknown <bala@enunui.com>
2018-04-28 10:08:45 +0530

    Associate users with todos

--- node-todo-api/server/tests/seed/seed.js
+++ node-todo-api/server/tests/seed/seed.js
@@ -14,22 +14,28 @@ const users = [{
     tokens: [{
         access: 'auth',
         token: jwt.sign({_id: userOneId,access: 'auth'}, 'abc123').toString()
-
-
-    }]
-},{
+      }]
+    },{
 _id: userTwoId,
 email: 'jen@example.com',
-password: 'userTwoPass'
+password: 'userTwoPass',
+tokens: [{
+  access: 'auth',
+  token: jwt.sign({_id: userTwoId,access: 'auth'}, 'abc123').toString()
+}]
 }];
 
 
  const todos = [{
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+    _creater: userOneId
   }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333,
+    _creator: userTwoId
   }];
   
   const populateTodos = (done) => {
